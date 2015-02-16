78508468de011ed1c06c485d05f4ad78d7cc1220
James Ryan <joryan@ucsc.edu>
2015-02-15 20:05:54 -0800

    Changed Character object to Person object (which better reflects the simulationist approach).

---
+++ person.py
@@ -0,0 +1,7 @@
+class Person(object):
+    """A person living in a city of a gameplay instance."""
+
+    def __init__(self, mother, father):
+        """Construct a Person object."""
+        self.mother = mother
+        self.father = father
