7f6d0ed72ad2cb838cea6fdfc12f97196066d5ba
James Ryan <joryan@ucsc.edu>
2015-02-17 21:03:32 -0800

    Added many more parameters.

--- config.py
+++ config.py
@@ -4,6 +4,30 @@ class Config(object):
     def __init__(self):
         """Construct a Config object."""
         self.n_buildings_per_block = 5
+                ## WORLD GEN ##
+        # Misc
+        self.year_city_gets_founded = 1909  # Year world gen begins
+        # Founding residents of the city
+        self.n_founding_families_mean = 5
+        self.n_founding_families_sd = 1
+        self.n_founding_families_floor = 3
+        self.n_founding_families_cap = 7
+        self.n_single_founders_mean = 4
+        self.n_single_founders_sd = 2
+        self.n_single_founders_floor = 2
+        self.n_single_founders_cap = 11
+        self.founding_mother_age_floor = 19
+        self.founding_mother_age_cap = 59
+        self.founding_father_age_floor = 19
+        self.founding_father_age_diff_floor = -2
+        self.founding_father_age_diff_cap = 8
+        self.founding_father_age_at_marriage_mean = 21
+        self.founding_father_age_at_marriage_sd = 2.7
+        self.founding_father_age_at_marriage_floor = 17
+        self.founding_mother_age_at_marriage_floor = 17
+                ## FULL SIMULATION ##
+        self.chance_newlyweds_keep_former_love_interests = 0.01
+                ## PEOPLE ##
         # Infertility
         self.male_infertility_rate = 0.07
         self.female_infertility_rate = 0.11
@@ -18,7 +42,7 @@ class Config(object):
         self.memory_floor = 0.1
         self.memory_floor_at_birth = 0.55  # Worst possible memory of newborn
         self.memory_sex_diff = 0.03  # Men have worse memory, studies show
-        self.memory_heritability = 0.6  # Couldn't quickly find a study on this
+        self.memory_heritability = 0.6  # Couldn't quickly find a study on this -- totally made up
         self.memory_heritability_sd = 0.05
         # Big Five personality traits (source Schmitt et al. "Big Five Traits
         # Across 56 Nations"; other papers for heritability)
