34462b0b3d289a152502a7418b1466ff5305e843
James Ryan <joryan@ucsc.edu>
2015-02-21 17:03:35 -0800

    Added Cemetery class.

--- landmark.py
+++ landmark.py
@@ -1,5 +1,23 @@
+# There are two types of landmarks: Cemetery and Park. The distinction
+# between a landmark and a business is that a landmark does not have a
+# building and is not owned by anyone.
+
+
+class Cemetery(object):
+    """A cemetery on a tract in a city."""
+
+    def __init__(self, block):
+        """Initialize a Cemetery object."""
+        self.game = block.city.game
+        self.city = block.city
+        self.street = block.street
+        self.block = block
+
+        # groundskeepers, mortician
+
+
 class Park(object):
-    """A park on a block in a city."""
+    """A park on a tract in a city."""
 
     def __init__(self, block):
         """Initialize a Park object."""
@@ -7,3 +25,5 @@ class Park(object):
         self.city = block.city
         self.street = block.street
         self.block = block
+
+        # groundskeepers
