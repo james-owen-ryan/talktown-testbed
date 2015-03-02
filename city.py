c126cdf0fe05e4c45ca5e4f8fed2ac26a6cd03b3
James Ryan <joryan@ucsc.edu>
2015-03-02 09:30:26 -0800

    Fixed Lots getting added into their own neighboring lots

--- city.py
+++ city.py
@@ -46,7 +46,7 @@ class City(object):
         self.generateLots(gameState.config)
         for lot in self.lots | self.tracts:
             lot.setNeighboringLots()
-            lot._init_generate_address()
+            lot.init_generate_address()
         self.paths = {}
         self.generatePaths()
         self.downtown = None
@@ -571,10 +571,10 @@ class Lot(object):
         self.landmark = None  # Will always be None for Lot
         self.positionsInBlock = []
         self.neighboring_lots = set()  # Gets set by City call to setNeighboringLots after all lots have been generated
-        # These get set by _init_generate_address()
+        # These get set by init_generate_address(), which gets called by City
         self.address = None
         self.street_address_is_on = None
-        # self._init_generate_address()
+        self.neighboring_residents = set([])  # Gets added to as appropriate by event.Move
 
     def addBlock(self, block, number, sideOfStreet, positionInBlock):
         self.streets.append(block.street)
@@ -587,10 +587,11 @@ class Lot(object):
         neighboringLots = set()
         for block in self.blocks:
             for lot in block.lots:
-                neighboringLots.add(lot)
+                if lot is not self:
+                    neighboringLots.add(lot)
         self.neighboring_lots = neighboringLots
 
-    def _init_generate_address(self):
+    def init_generate_address(self):
         """Generate an address, given the lot building is on."""
         index_of_street_address_will_be_on = random.randint(0, len(self.streets)-1)
         house_number = self.house_numbers[index_of_street_address_will_be_on]
@@ -598,15 +599,6 @@ class Lot(object):
         street = self.streets[index_of_street_address_will_be_on]
         self.address = "{0} {1}".format(house_number, street.name)
         self.street_address_is_on = street
-
-    @property
-    def population(self):
-        """Return the number of people living/working on the lot."""
-        if self.building:
-            population = len(self.building.residents)
-        else:
-            population = 0
-        return population
 
 
 class Tract(Lot):
