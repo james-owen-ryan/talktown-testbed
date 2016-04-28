1cf555f2acaed0b6a46c5e030c105a80292114fd
jillzy <jillzyeung@gmail.com>
2016-04-28 15:07:35 -0700

    refactor parse and render lots

--- js/client.js
+++ js/client.js
@@ -1,21 +1,14 @@
-var i = 0;
+/********************************************
+*											*
+*		parse and render lots				*
+*											*
+********************************************/
 
-/*
-function parseLotsJson(json){
-	var dict = JSON.parse(json);
-	for(var key in dict) {
-		var value = dict[key];
-		console.log("["+key+value+"]");
-		var sprite = game.add.sprite(i, 0, 'phaser');
-		i = i + 30;
- 
-	}
-}
-*/
+var i = 0;
 var coordinates;
 var xCoord;
 var yCoord;
-var i = 1;
+
 function parseLotsJson(json){
 	var dict = JSON.parse(json);
 	for(var key in dict) {
@@ -25,19 +18,37 @@ function parseLotsJson(json){
 		coordinates = key.split(', ');
 		xCoord = coordinates[0].substring(1, coordinates[0].length - 1);
 		yCoord = coordinates[1].substring(0, coordinates[1].length - 2);
-		game.load.start()
-		var sprite = game.add.sprite(xCoord*(window.innerWidth/12) + 5, yCoord*(window.innerHeight/12), 'house');
-		//var sprite = game.add.sprite(i*(window.innerWidth/18), 30, 'phaser');}
-		//i++;
-		//console.log(i);
+		renderLots(xCoord, yCoord, value);
 	}
 }
 
+function renderLots(x, y, value){
+	if (value == "House") { 
+		var sprite = game.add.sprite(x*(window.innerWidth/9)-((window.innerWidth/9)/2), y*(window.innerHeight/9)-((window.innerHeight/9)/2), 'house');
+	} else {
+		var sprite = game.add.sprite(x*(window.innerWidth/9)-((window.innerWidth/9)/2), y*(window.innerHeight/9)-((window.innerHeight/9)/2), 'empty_lot');
+	}
+}
+
+//render json lots function
+//render streets function
+
+
+
+/********************************************
+*											*
+*		load house, empty ,and business		*
+*		lots sprites						*
+*											*
+********************************************/
+
 function preload() {
+	
 	game.load.baseURL = 'http://examples.phaser.io/assets/';
 	game.load.crossOrigin = 'anonymous';
-	game.load.image('house', 'sprites/phaser-dude.png');
-	
+	game.load.image('empty_lot', 'sprites/phaser-dude.png');
+
+	game.load.image('house', 'Sprites/house.png');
 
 
 }
